cfc81cdc7a2c1d3b61fd7d75d6fa349ca59f1b4b
Sreenidhi Palimar <bhatshrinidhi10@gmail.com>
2023-05-02 18:04:28 +0530

    5 times print

--- Demo.java
+++ Demo.java
@@ -3,5 +3,9 @@ class Demo
 public static void main(String[] args)
 {
 System.out.println("Hello Java");
+System.out.println("Hello Java");
+System.out.println("Hello Java");
+System.out.println("Hello Java");
+System.out.println("Hello Java");
 }
 }
